6d939d9169b7290c21c1b0f7d84933c5841d3871
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2023-10-19 19:46:30 +0200

    Added c# eventgrid demo

---
+++ Events/C#/eventgrid/Program.cs
@@ -0,0 +1,26 @@
+﻿using Azure;
+using Azure.Messaging.EventGrid;
+
+
+class Program
+{
+    static async Task Main()
+    {
+        string endpoint = "YOUR_EVENT_GRID_ENDPOINT";
+        string topicAccessKey = "YOUR_TOPIC_KEY";
+
+        var client = new EventGridPublisherClient(
+            new Uri(endpoint), 
+            new AzureKeyCredential(topicAccessKey));
+
+        var sampleEvent = new EventGridEvent(
+            "sampleEventSubject",
+            "sampleEventType",
+            "1.0",
+            new BinaryData("{ \"data\": \"sampleEventData\" }"));
+
+        await client.SendEventsAsync(new EventGridEvent[] { sampleEvent });
+
+        Console.WriteLine("Event published!");
+    }
+}
