21625c5fd92ce4c7740f3a40e5422bd001b6c751
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2023-10-19 22:00:29 +0200

    Added c# eventhubs demo

---
+++ Events/C#/eventhub/receiver/Program.cs
@@ -0,0 +1,59 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+
+class Receiver
+{
+    private const string connectionString = "";
+    private const string eventHubName = "";
+    private const string storageConnectionString = "";
+    private const string storageContainerName = "";
+
+    static async Task Main(string[] args)
+    {
+        var blobServiceClient = new BlobServiceClient(storageConnectionString);
+        var blobContainerClient = blobServiceClient.GetBlobContainerClient(storageContainerName);
+
+        var processorClient = new EventProcessorClient(
+            blobContainerClient, 
+            EventHubConsumerClient.DefaultConsumerGroupName, 
+            connectionString, 
+            eventHubName);
+
+        processorClient.ProcessEventAsync += ProcessEventHandler;
+        processorClient.ProcessErrorAsync += ProcessErrorHandler;
+
+        Console.WriteLine("Starting event processor");
+        await processorClient.StartProcessingAsync();
+
+        Console.WriteLine("Press enter to stop.");
+        Console.ReadLine();
+
+        Console.WriteLine("Stopping event processor");
+        await processorClient.StopProcessingAsync();
+
+        Console.WriteLine("Event processor stopped.");
+        Console.WriteLine("Exiting process");
+    }
+
+    private static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+    {
+        var partitionContext = eventArgs.Partition;
+        var eventData = eventArgs.Data;
+
+        Console.WriteLine($"Processing event from partition {partitionContext.PartitionId} with sequence number {eventData.SequenceNumber} with body: {eventData.EventBody}");
+
+        // Every 10 events received, it will update the checkpoint stored in Azure Blob Storage.
+        if (eventData.SequenceNumber % 10 == 0)
+        {
+            await eventArgs.UpdateCheckpointAsync();
+        }
+    }
+
+    private static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+    {
+        Console.WriteLine($"Error occurred in partition processor for partition {eventArgs.PartitionId}, {eventArgs.Exception.Message}");
+        return Task.CompletedTask;
+    }
+}
---
+++ Events/C#/eventhub/sender/Program.cs
@@ -0,0 +1,50 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+const string connectionString = "";
+const string eventHubName = "";
+
+// Create a producer client
+await using var producerClient = new EventHubProducerClient(connectionString, eventHubName);
+
+// Sample events in a list
+var allEvents = new List<EventData>
+{
+    new EventData("Foo"),
+    new EventData("Bar")
+};
+
+// More data
+for (int i = 0; i < 10; i++)
+{
+    allEvents.Add(new EventData("Event-" + i));
+}
+
+// Create a batch
+using EventDataBatch eventDataBatch = await producerClient.CreateBatchAsync();
+
+foreach (EventData eventData in allEvents)
+{
+    // Try to add the event from the list to the batch
+    if (!eventDataBatch.TryAdd(eventData))
+    {
+        // If the batch is full, send it and then create a new batch
+        await producerClient.SendAsync(eventDataBatch);
+        
+        eventDataBatch.Dispose();
+
+        // Try to add the event that couldn't fit before
+        if (!eventDataBatch.TryAdd(eventData))
+        {
+            throw new ArgumentException(
+                "Event is too large for an empty batch. Max size: " + eventDataBatch.MaximumSizeInBytes);
+        }
+    }
+}
+
+// Send the last batch of remaining events
+if (eventDataBatch.Count > 0)
+{
+    Console.WriteLine($"Sending {eventDataBatch.Count} events");
+    await producerClient.SendAsync(eventDataBatch);
+}
